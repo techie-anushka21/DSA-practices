5633ddd2920f7077846c37a42d4fcea0ec1ad98d
Anushka Singh <ms.anushka2108@gmail.com>
2026-03-10 21:10:45 +0530

    Accessing all/multiple Array Elements using for loop- Part 2

---
+++ ArrayWithLoops2.java
@@ -0,0 +1,18 @@
+/*Accessing all the Array elements together using for loop- Part 2 (String data) */
+public class ArrayWithLoops2
+{
+    public static void main(String args[])
+    {
+        String[] names = new String[4];    //array creation
+        names[0] = "Ajit";       //names value for 1st index;
+        names[1] = "Aadya";      //names value for 2nd index;
+        names[2] = "Saurabh";    //names value for 3rd index; 
+        /*names value for 4th index not assigned- upon accessing,
+         we get index 3's values as "null" */
+        
+        for(int i=0; i<4; i++)    //for loop to print names of all the 3 People
+        {
+            System.out.println(names[i]);
+        }
+    }    
+}
