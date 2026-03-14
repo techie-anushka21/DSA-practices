cc89a84a49d34edc77e685032f85b20a1cdd8a34
Anushka Singh <ms.anushka2108@gmail.com>
2026-03-14 22:34:56 +0530

    Finding the Largest Element in an Array

---
+++ MaxArrayElement.java
@@ -0,0 +1,46 @@
+/*Finding the Largest (max) element in an Array */
+
+import java.util.Scanner;    //importing the Scanner class
+public class MaxArrayElement
+{
+    public static void main(String args[])
+    {
+        /*Taking size of the Array from the User */
+        System.out.print("Enter the size of the Array: ");
+        Scanner scn = new Scanner(System.in);
+        int size = scn.nextInt();    //Scanning the size input from User
+
+        int[] ar = new int[size];    //Array Creation
+
+        /*Taking Array elements from the User */
+        System.out.print("Enter the Array Elements: ");
+        for(int i=0; i<size; i++)
+        {
+            ar[i] = scn.nextInt();    //Scanning the Elements input
+        }
+
+        /*Printing the Array */
+        System.out.println("Array:-");
+        for(int i=0; i<size; i++)
+        {
+            System.out.print(ar[i] + " ");
+        }
+        System.out.println();
+
+        /*Finding the largest(max) element */
+        int max= ar[0];    //Assume the 1st element at 0th index as max
+        for(int i=0; i<size; i++)    //Use for loop so that every element is checked
+        {
+            if(ar[i]> max)    /*using if statement, check if any element is greater than 
+                                the assumed max element */
+            {
+                max = ar[i];  /*if yes, then assign that element to the max variable */
+            }
+        }
+        System.out.println("Largest Element of the Array = " + max);
+
+        scn.close();
+    }
+}
+/*1. Time Complexity = O(n)
+  2. Space Complexity = O(n) + O(1) = O(n) */
